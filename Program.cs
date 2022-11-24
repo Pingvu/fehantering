000c9ca47cf637a1272699392c0bcda4c3eb4075
Pingvu <nhlpool27@gmail.com>
2022-11-24 12:29:58 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿static void Main (string[] args){
+int resultat;
+if(!int.TryParse(Console.ReadLine(), out resultat))
+Console.WriteLine("skriv in en siffra");
+
+}
+ static bool TryParse(string s, out int result){
+            return true;
+            
+        
+        }
+
