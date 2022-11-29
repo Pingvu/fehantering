de4c5ecba99e64ed99de45b911961726784bc77b
Pingvu <nhlpool27@gmail.com>
2022-11-29 11:33:52 +0100

    up

--- Program.cs
+++ Program.cs
@@ -1,17 +1,20 @@
 ﻿class Program{
 static void Main (string[] args){
 int resultat;
-string s = Console.ReadLine();
 if(!int.TryParse(Console.ReadLine(), out resultat))
 Console.WriteLine("skriv in en siffra");
-
 }
  static bool TryParse(string s, out int result){
-         if (int.TryParse(s, out result))
+    
+         try
          {
+            result= Convert.ToInt32(s);
             return true;
          }
-         return false;
+         catch (System.Exception)
+         {
+            return false;
+         }
         }
 }
 
